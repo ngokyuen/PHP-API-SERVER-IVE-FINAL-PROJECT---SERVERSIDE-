d0d16d65653fa473c198b8f06b93a7beaac26428
ngokyuen <tedngok@gmail.com>
2016-03-07 02:29:54 +0800

    update orders sql structure

--- sql/orders.sql
+++ sql/orders.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Mar 06, 2016 at 06:44 PM
+-- Generation Time: Mar 06, 2016 at 07:29 PM
 -- Server version: 5.5.42
 -- PHP Version: 7.0.0
 
@@ -23,14 +23,16 @@ SET time_zone = "+00:00";
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
-  `type` varchar(10) COLLATE utf8_bin NOT NULL COMMENT 'normal: normal order, share:car share order',
-  `status` varchar(10) COLLATE utf8_bin NOT NULL COMMENT 'pending, completed, cancel',
+  `type` varchar(10) COLLATE utf8_bin NOT NULL DEFAULT 'normal' COMMENT 'normal: normal order, share:car share order',
+  `status` varchar(10) COLLATE utf8_bin NOT NULL DEFAULT 'pending' COMMENT 'pending, completed, cancel',
   `origin` varchar(255) COLLATE utf8_bin NOT NULL,
   `destination` varchar(255) COLLATE utf8_bin NOT NULL,
-  `orgin_remark` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `origin_remark` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `destination_remark` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `book_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `passenger` int(11) NOT NULL COMMENT 'number of passenger',
+  `contact_person` varchar(255) COLLATE utf8_bin NOT NULL,
+  `contact_no` int(8) NOT NULL,
   `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified_date` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
