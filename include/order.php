e1b33905ff21ea38f9477f0f164ee274a9508d74
l4d40910 <l4d40910@gmail.com>
2016-03-10 22:09:29 +0800

    add 2 function in order.php
    2016-03-10
    + case "removeOrder"
    + case "getGeneralOrder" with limit 5 orders

--- include/order.php
+++ include/order.php
@@ -46,6 +46,55 @@ class Job {
         else
             return array("result"=>false);
     }
+	
+	//2016-03-09
+	//remove function
+	function removeOrder(){ 
+		$id = isset($_POST['id']) ? mysqli_real_escape_string($this->SQL, $_POST['id']) : "";
+		
+		//non-finish validation
+		//switch case
+		$query = "UPDATE orders SET status ='cancel' where id = '$id';";
+		$result = mysqli_query($this->SQL, $query);
+        
+        if ($order_id = mysqli_insert_id($this->SQL))
+            return array("result"=>true);
+        else
+            return array("result"=>false);
+    }
+	
+	//2016-03-10
+	//get General Order
+	function getGeneralOrder(){
+		$token = isset($_POST['token']) ? mysqli_real_escape_string($this->SQL, $_POST['token']) : ""; //client send
+		$type = isset($_POST['type']) ? mysqli_real_escape_string($this->SQL, $_POST['type']) : ""; //client send
+		
+		//get token
+		if ($token != ""){
+            $query = "SELECT * FROM users WHERE token ='$token';";
+            $result = mysqli_query($this->SQL, $query);
+            $row = mysqli_fetch_assoc($result);
+            //$user_id = $row["id"];   
+        }
+		
+		//loop get data limit 5 orders
+		$query = "SELECT id, type, book_date, origin, destination, passenger, status FROM orders Order BY book_date LIMIT 5;";
+		if ($result = mysqli_query($this->SQL, $query)) {
+			return array ("result"=>true);
+			//loop begin
+			while ($order_id = mysqli_fetch_field($result)) {
+				return array("id"=>$id);
+				return array("type"=>$type);
+				return array("book_date"=>$book_date);
+				return array("origin"=>$origin);
+				return array("destination"=>$destination);
+				return array("passenger"=>$passenger);
+				return array("status"=>$status);
+		}
+			mysqli_free_result($result);
+        else
+            return array("result"=>false);
+	}
 }
 
 //response variable
@@ -58,6 +107,13 @@ if (isset($_POST["action2"])){
         case "addOrder":
             $response = $job->addOrder();
         break;
+		case "removeOrder" //2016-03-09
+			$response = $job->removeOrder();
+		break;
+		case "getGeneralOrder"; //2016-03-10
+			$response = $job->getGeneralOrder();
+		break;
+			
     }
 }
 
