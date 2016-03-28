fd8e2ada485fd490260646a5373eaa280abd0fd5
ngokyuen <tedngok@gmail.com>
2016-03-28 22:02:21 +0800

    modify orders sql structure for lat lng

--- sql/orders.sql
+++ sql/orders.sql
@@ -1,27 +1,33 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.10
+-- version 4.1.13
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:3306
--- Generation Time: Mar 06, 2016 at 07:29 PM
--- Server version: 5.5.42
--- PHP Version: 7.0.0
+-- 主機: localhost
+-- 產生時間： 2016 年 03 月 28 日 22:00
+-- 伺服器版本: 5.5.44-MariaDB-log
+-- PHP 版本： 5.5.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 --
--- Database: `project3b`
+-- 資料庫： `project3b`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `orders`
+-- 資料表結構 `orders`
 --
 
-CREATE TABLE `orders` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL,
   `type` varchar(10) COLLATE utf8_bin NOT NULL DEFAULT 'normal' COMMENT 'normal: normal order, share:car share order',
   `status` varchar(10) COLLATE utf8_bin NOT NULL DEFAULT 'pending' COMMENT 'pending, completed, cancel',
@@ -33,26 +39,15 @@ CREATE TABLE `orders` (
   `passenger` int(11) NOT NULL COMMENT 'number of passenger',
   `contact_person` varchar(255) COLLATE utf8_bin NOT NULL,
   `contact_no` int(8) NOT NULL,
+  `origin_lat` float DEFAULT NULL,
+  `origin_lng` float DEFAULT NULL,
+  `destination_lat` float DEFAULT NULL,
+  `destination_lng` float DEFAULT NULL,
   `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified_date` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `modified_date` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=7 ;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `orders`
---
-ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
